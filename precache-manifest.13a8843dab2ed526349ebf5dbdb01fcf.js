a5c2fe85f8c531adde2e04aa25ca3786d8ca39f4
Edgar Gonzalez <Edggar@Gmail.com>
2020-09-07 22:55:52 -0400

    Updates

---
+++ precache-manifest.13a8843dab2ed526349ebf5dbdb01fcf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90dda3ac4c5b16cf88fdc2814af29480",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d71a0ee4ddb2927a4790",
+    "url": "/static/css/main.57fd2515.chunk.css"
+  },
+  {
+    "revision": "4643086dff2a7f46bb8d",
+    "url": "/static/js/2.2ca30f94.chunk.js"
+  },
+  {
+    "revision": "3df497401d324ee6ed76d4d98715bc5f",
+    "url": "/static/js/2.2ca30f94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d71a0ee4ddb2927a4790",
+    "url": "/static/js/main.055a408a.chunk.js"
+  },
+  {
+    "revision": "efbc31353b54c94c413e",
+    "url": "/static/js/runtime-main.d3b4b08a.js"
+  }
+]);
